2a0b319522884722e2fc854b68bdeec33ae66d07
mariocairone <mario.cairone@protonmail.com>
2020-07-30 17:12:48 +0100

    Added version 4.2.2 to past versions

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ docker login --username mariocairone --password $DOCKER_PASSWORD
 
 echo "*************************************************************************************************"
 
-for VERSION in 3.8.0 3.8.1 3.8.2 3.8.3 3.8.4 3.8.5 3.8.6 3.8.7 3.9.0 3.9.1 3.9.2 3.9.3 4.2.0 4.2.1
+for VERSION in 3.8.0 3.8.1 3.8.2 3.8.3 3.8.4 3.8.5 3.8.6 3.8.7 3.9.0 3.9.1 3.9.2 3.9.3 4.2.0 4.2.1 4.2.2
 do
 	echo "*************************************************************************************************"
     echo "Building image for runtime version ${VERSION}"
@@ -19,7 +19,4 @@ do
 	docker push mariocairone/mule-ee:${VERSION} 
 	echo "Image mariocairone/mule-ee:${VERSION} pushed to Docker Hub"
 	echo "*************************************************************************************************"	
-done
-
-       
-    
+done   
